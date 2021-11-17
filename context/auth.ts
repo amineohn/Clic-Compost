019da4cd5b22a2f5e9210af35d18ba60aee1a36b
amineohn <amineprojet7@gmail.com>
2021-11-17 12:02:58 +0100

    init firebase app and use asynchrone.

--- context/auth.ts
+++ context/auth.ts
@@ -1,22 +1,36 @@
 import { useState, useEffect } from "react";
 import Auth, { getAuth } from "@firebase/auth";
+import { initializeApp } from "firebase/app";
 const formatAuthUser = (user) => ({
   uid: user.uid,
   email: user.email,
 });
 
-export default function authentification() {
+export default async function authentification() {
   const [authUser, setAuthUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const auth = getAuth();
 
-  const signInWithEmailAndPassword = (email: string, password: string) =>
-    Auth.signInWithEmailAndPassword(auth, email, password);
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_8go5RZ0kTpp19ZUkiVFGeTUC8fvYDWs",
+    authDomain: "clickncompost.firebaseapp.com",
+    projectId: "clickncompost",
+    storageBucket: "clickncompost.appspot.com",
+    messagingSenderId: "1020295512270",
+    appId: "1:1020295512270:web:707f016bc4c0a310afd09f",
+    measurementId: "G-R7P9BDSQJD",
+  };
+  await initializeApp(firebaseConfig);
 
-  const createUserWithEmailAndPassword = (email: string, password: string) =>
-    Auth.createUserWithEmailAndPassword(auth, email, password);
+  const signInWithEmailAndPassword = async (email: string, password: string) =>
+    await Auth.signInWithEmailAndPassword(auth, email, password);
 
-  const signOut = () => Auth.signOut(auth).then();
+  const createUserWithEmailAndPassword = async (
+    email: string,
+    password: string
+  ) => await Auth.createUserWithEmailAndPassword(auth, email, password);
+
+  const signOut = async () => await Auth.signOut(auth).then();
 
   const authStateChanged = async (authState: any) => {
     if (!authState) {
@@ -33,7 +47,7 @@ export default function authentification() {
 
   useEffect(() => {
     const unsubscribe = Auth.onAuthStateChanged(auth, authStateChanged);
-    return () => unsubscribe();
+    return unsubscribe();
   }, []);
 
   return {
