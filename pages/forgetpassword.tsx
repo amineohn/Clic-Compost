6735a9756d12cdd1cbef67df7472e107e8ad684d
amineohn <amineprojet7@gmail.com>
2021-11-25 09:58:47 +0100

    style(forgetpass): format code. feat: add login with google.

--- pages/forgetpassword.tsx
+++ pages/forgetpassword.tsx
@@ -42,65 +42,62 @@ const forgetPassword: NextPage = () => {
 
   return (
     <>
-      <FadeIn>
-        <div className="flex flex-col items-center justify-center h-screen">
-          <div className="w-full max-w-xs space-y-2">
-            {error && (
-              <FadeIn className="bg-red-100 border border-red-100 text-red-700 px-4 py-3 rounded-lg relative space-y-2 overflow-auto">
-                <strong className="font-bold">Erreur</strong>
-                <span className="block sm:inline">{error}</span>
-                <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
-              </FadeIn>
-            )}
+      <FadeIn className="flex flex-col items-center justify-center h-screen">
+        <div className="w-full max-w-xs space-y-2">
+          {error && (
+            <FadeIn className="bg-red-100 border border-red-100 text-red-700 px-4 py-3 rounded-lg relative space-y-2 overflow-auto">
+              <strong className="font-bold">Erreur</strong>
+              <span className="block sm:inline">{error}</span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
+            </FadeIn>
+          )}
 
-            {success && (
-              <FadeIn className="bg-green-100 border border-green-100 text-green-700 px-4 py-3 rounded-lg relative space-y-2">
-                <strong className="font-bold">Success!</strong>
-                <span className="block sm:inline">
-                  Nous vous avons envoyé un e-mail pour réinitialiser votre mot
-                  de passe.
-                </span>
-                <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
-              </FadeIn>
-            )}
+          {success && (
+            <FadeIn className="bg-green-100 border border-green-100 text-green-700 px-4 py-3 rounded-lg relative space-y-2">
+              <strong className="font-bold">Success!</strong>
+              <span className="block sm:inline">
+                Nous vous avons envoyé un e-mail pour réinitialiser votre mot de
+                passe.
+              </span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
+            </FadeIn>
+          )}
 
-            <div className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-              <div className="mb-4">
-                <h1 className="text-center text-2xl font-bold">
-                  Mot de passe oublié
-                </h1>
-                <p className="text-center text-gray-700 text-xs">
-                  Entrez votre adresse email pour réinitialiser votre mot de
-                  passe
-                </p>
-              </div>
-              <form onSubmit={forgetPassword}>
-                <div className="mb-4">
-                  <label
-                    className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="email"
-                  >
-                    Email
-                  </label>
-                  <input
-                    className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="flex items-center justify-between">
-                  <button
-                    className="bg-greenDDTV hover:bg-green-800 text-white font-bold rounded-lg py-2 px-4 focus:outline-none focus:shadow-outline"
-                    type="submit"
-                  >
-                    {loading ? <Loading message="Chargement" /> : "Envoyer"}
-                  </button>
-                </div>
-              </form>
+          <div className="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <h1 className="text-center text-2xl font-bold">
+                Mot de passe oublié
+              </h1>
+              <p className="text-center text-gray-700 text-xs">
+                Entrez votre adresse email pour réinitialiser votre mot de passe
+              </p>
             </div>
+            <form onSubmit={forgetPassword}>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-greenDDTV hover:bg-green-800 text-white font-bold rounded-lg py-2 px-4 focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  {loading ? <Loading message="Chargement" /> : "Envoyer"}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </FadeIn>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,11 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import fb from "firebase/compat/app";
+import "firebase/compat/auth";
 import { useRouter } from "next/router";
 import Loading from "../components/Loading";
 import Link from "next/link";
+
 const Home: NextPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -18,11 +20,22 @@ const Home: NextPage = () => {
   const [success, setSuccess] = useState(false);
   const router = useRouter();
   const auth = getAuth();
-  // if user is logged in, success will be true and we will redirect to /dashboard  page automatically
-
+  const provider = new GoogleAuthProvider();
   if (auth && auth.currentUser) {
     router.push("/collect");
   }
+
+  // authenticate user with google sign-in
+  const authenticateWithGoogle = async () => {
+    try {
+      setLoading(true);
+      await fb.auth().signInWithPopup(provider);
+      setLoading(false);
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
 
   const setEmailChange = (e: FormEvent<HTMLInputElement>) => {
     setEmail(e.currentTarget.value);
@@ -113,6 +126,7 @@ const Home: NextPage = () => {
     }
     setLoading(false);
   };
+  // button to connect with google account tailwindcss
 
   return (
     <>
@@ -188,6 +202,27 @@ const Home: NextPage = () => {
                 </Link>
               </div>
             </form>
+
+            <div className="mt-5 flex">
+              <button
+                className="flex bg-greenDDTV hover:bg-green-800 text-white font-normal py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                onClick={() => authenticateWithGoogle()}
+              >
+                <svg
+                  className="h-4 w-4 mr-2 mt-1 fill-current !text-white"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g transform="matrix(1, 0, 0, 1, 27.009001, -39.238998)">
+                    <path d="M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z" />
+                    <path d="M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z" />
+                    <path d="M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z" />
+                    <path d="M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z" />
+                  </g>
+                </svg>
+                Connexion avec Google
+              </button>
+            </div>
           </div>
         </div>
       </FadeIn>
