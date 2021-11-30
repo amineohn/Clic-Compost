5eba001de0146491741d7e70d37b33712ea584d2
amineohn <amineprojet7@gmail.com>
2021-11-30 12:30:19 +0100

    blank space

--- pages/index.tsx
+++ pages/index.tsx
@@ -251,7 +251,7 @@ const Home: NextPage = () => {
               <button
                 type="button"
                 onClick={() => authenticateWithGoogle()}
-                className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                className="py-2 px-4 flex justify-center items-center bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
               >
                 <svg
                   width="20"
