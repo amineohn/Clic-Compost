91e51c14edae7239f5b11b61bfc131de337e5d13
amineohn <amineprojet7@gmail.com>
2021-12-01 14:43:02 +0100

    fix: nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import { Firebase } from "../libs/firebase";
 import { NextSeo } from "next-seo";
 import { Router } from "next/router";
 import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const Navigation = dynamic(() => import("../components/navigation"), {
   ssr: false,
@@ -17,7 +18,6 @@ const Navigation = dynamic(() => import("../components/navigation"), {
 const fire = new Firebase();
 const isAvailable = Capacitor.isPluginAvailable("StatusBar");
 const isAvailable2 = Capacitor.isPluginAvailable("SplashScreen");
-
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
