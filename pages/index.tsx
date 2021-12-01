426544b8103e33a716e617556ecc6ec65bb13968
amineohn <amineprojet7@gmail.com>
2021-12-01 11:04:01 +0100

    change loader too

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,26 +81,8 @@ const Home: NextPage = () => {
         {redirection && (
           <div className="flex items-center justify-center z-50">
             <div className="flex justify-center m-auto space-x-2">
-              <svg
-                className="flex justify-center animate-spin h-7 w-7 text-gray-800 mt-1"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                />
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                />
-              </svg>
+              <div className="animate-spin rounded-full h-7 w-7 border-t-2 border-b-2 border-gray-900" />
+
               <span className="flex text-center text-lg font-medium m-auto">
                 Redirection...
               </span>
