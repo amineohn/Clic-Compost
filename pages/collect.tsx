a6aa6569906c41d2977d4f5b37c99eba4540ec1c
amineohn <amineprojet7@gmail.com>
2021-12-01 10:57:06 +0100

    added message if no collect is added, change loading page

--- pages/collect.tsx
+++ pages/collect.tsx
@@ -399,6 +399,7 @@ const Collect: NextPage = () => {
             </div>
           </div>
         </div>
+
         <div className="flex justify-center">
           <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 lg:w-96 w-72">
             <div className="flex flex-col">
@@ -408,6 +409,34 @@ const Collect: NextPage = () => {
             </div>
           </div>
         </div>
+        {data.length !== 0 && (
+          <>
+            <FadeIn>
+              <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 my-5">
+                <div className="flex items-center justify-center w-12 bg-greenDDTV">
+                  <svg
+                    className="w-6 h-6 text-white fill-current"
+                    viewBox="0 0 40 40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+                  </svg>
+                </div>
+
+                <div className="px-4 py-2 -mx-3">
+                  <div className="mx-3">
+                    <span className="font-semibold text-greenDDTV dark:text-greenDDTV">
+                      Attention
+                    </span>
+                    <p className="text-sm text-gray-600 dark:text-gray-200">
+                      Aucune collecte n'as été crée.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </FadeIn>
+          </>
+        )}
       </FadeIn>
     </>
   );
