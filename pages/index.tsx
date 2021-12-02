c61d3f5a6d612b41512d4697dd4ff825149a794c
amineohn <amineprojet7@gmail.com>
2021-12-02 10:40:18 +0100

    fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -106,9 +106,9 @@ const Home: NextPage = () => {
             className={`fill-current text-black w-36 h-36 ${
               error &&
               (error.length > 0
-                ? "text-red-500 animate-bounce transition duration-200"
+                ? "!text-red-500 animate-bounce transition duration-200"
                 : "")
-            } ${success && (success ? "text-green-500 transition" : "")}`}
+            } ${success && (success ? "!text-green-500 transition" : "")}`}
             onClick={() => router.push("/")}
           >
             <path d="M96.16,6.73c-.72-1.82-1.72-4.24-4-4.34-2.68-.12-4.33,2.16-5.11,3.68a21.72,21.72,0,0,0-2,12c.46,2.79,3.71,3.39,4.29,5.08s.07,7-.21,8.3c0,0,1.53-2.31,3.86-1.94,0-2.28-.75-4.47-.19-6.74.41-1.65,2.57-2.57,3.62-4.41,1.88-3.31.82-8.68-.34-11.62M92.72,20.54c1.52-2.06,3.94-4.23,3-7.82,0-.25.16-.3.16-.5l.5-.08c.69,3.73-.71,7.07-3.61,8.4" />
