ab344eca4632462db034dd85740b2125653790e4
amineohn <amineprojet7@gmail.com>
2021-12-02 10:09:46 +0100

    fixes/ imp

--- pages/collect.tsx
+++ pages/collect.tsx
@@ -121,7 +121,6 @@ const Collect: NextPage = () => {
       setSuccess(false);
     }
   };
-
   const clear = () => {
     setPhone("");
     setName("");
@@ -148,21 +147,6 @@ const Collect: NextPage = () => {
           const collectTime = data.collectTime;
           const frequency = data.frequency;
           const date = data.date;
-          if (doc.data().name === name) {
-            setError("Vous avez déjà utiliser ce nom");
-            setLoading(true);
-            setTimeout(() => {
-              setLoading(false);
-            }, 150);
-          }
-
-          if (doc.data().email === email) {
-            setError("Vous avez déjà utiliser cet email");
-            setLoading(true);
-            setTimeout(() => {
-              setLoading(false);
-            }, 150);
-          }
           setData((prev) => [
             ...prev,
             {
@@ -514,7 +498,7 @@ const Collect: NextPage = () => {
               </div>
             </FadeIn>
           )}
-          {data.length !== 0 && (
+          {data.length < 0 && (
             <>
               <FadeIn>
                 <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 my-5">
@@ -542,6 +526,30 @@ const Collect: NextPage = () => {
               </FadeIn>
             </>
           )}
+          <FadeIn>
+            <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 my-5">
+              <div className="flex items-center justify-center w-12 bg-orange-500">
+                <svg
+                  className="w-6 h-6 text-white fill-current"
+                  viewBox="0 0 40 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z" />
+                </svg>
+              </div>
+
+              <div className="px-4 py-2 -mx-3">
+                <div className="mx-3">
+                  <span className="font-semibold text-orange-500">
+                    Avertissement
+                  </span>
+                  <p className="text-sm text-gray-600 dark:text-gray-200">
+                    Cette version de Clic Compost est en cours de développement.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </FadeIn>
         </div>
       </FadeIn>
     </>
